4ded3d169244cb9d24fe9930b0e2f756978769b4
ishekapuri <ivanshekapuri2@gmail.com>
2022-04-21 12:42:46 -0700

    attempted to complete the display of all contact cards

---
+++ Downloads/1620_Exam_2022-main/1620_Exam_2022-main/README.md
@@ -0,0 +1,31 @@
+# 1620_Exam_2022
+1620 Exam Instructions and starter code
+
+
+## Project Assets
+
+### Colors
+
+![colors](./assets/exam.png)
+
+### Icons
+
+[font awesome](https://fontawesome.com/)
+
+#### Example adding an icon
+
+```HTML
+  <span class="fa-solid fa-ghost"></span>
+```
+
+### Type
+
+Sans-serif Inter `font-family: 'Inter', sans-serif;`
+weights: ExtraLight 200 and Medium 500
+
+#### Example use
+
+```CSS
+  font-family: 'Inter', sans-serif;
+  font-weight: 200;
+```
---
+++ Downloads/1620_Exam_2022-main/1620_Exam_2022-main/src/app.js
@@ -0,0 +1,78 @@
+// contact list, don't change this.
+const contactsList = [
+  {
+    name: "Togusa",
+    phone: "555 555-5555",
+    image: "togusa.png",
+    email: "togusa@secnine.com",
+    ID: "0"
+  },
+  {
+    name: "Tachikoma",
+    phone: "555 555-5555",
+    image: "tachikoma.png",
+    email: "tachikoma@secnine.com",
+    ID: "1"
+  },
+  {
+    name: "Saito",
+    phone: "555 555-5555",
+    image: "saito.png",
+    email: "saito@secnine.com",
+    ID: "2"
+  },
+  {
+    name: '"major" Motoko',
+    phone: "555 555-5555",
+    image: "motoko.png",
+    email: "motoko@secnine.com",
+    ID: "3"
+  },
+  {
+    name: "Ishikawa",
+    phone: "555 555-5555",
+    image: "ishikawa.png",
+    email: "ishikawa@secnine.com",
+    ID: "4"
+  },
+  {
+    name: "Batou",
+    phone: "555 555-5555",
+    image: "batou.png",
+    email: "batou@secnine.com",
+    ID: "5"
+  },
+  {
+    name: "Aramaki",
+    phone: "555 555-5555",
+    image: "aramaki.png",
+    email: "aramaki@secnine.com",
+    ID: "6"
+  },
+]
+
+
+
+// Your code goes here
+
+
+
+
+//id
+const cards = `<textarea id ="textArea"rows="30" cols="60"></textarea>`
+const displayAllContacts = document.querySelector(".display_all_contacts")
+
+
+//for (const contacts in contactsList) {
+  //displayAllContacts.insertAdjecentHtml("afterbegin", cards)
+  //cards.innerText = "cards.name";
+//}
+
+function display_contacts() {
+  for (const contacts in contactsList) {
+    const clickContactCard = document.createElement("button")
+    clickContactCard.innerHTML = contacts.name
+}
+}
+
+display_contacts()
